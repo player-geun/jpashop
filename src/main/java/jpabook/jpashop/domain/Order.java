cce1e3ef297cdf52cb03bcf58c015e24e6aded05
player-geun <player_geun@naver.com>
2022-09-24 11:05:15 +0900

    init

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+//DB마다 다르지만, ORDER가 예약어로 걸려있는 곳이 있어서 테이블명으로 ORDERS를 자주 사용
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+
+    //ORDER 사용하면 순서가 꼬여서 나중에 큰 장애 발생가능성
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
